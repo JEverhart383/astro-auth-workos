08ea599427f54fe12623c9cd806b398d5cf6d49b
Jeff Everhart <jeffeverhart383@gmail.com>
2024-04-20 20:21:36 -0400

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
-  integrations: [react()],
+  adapter: vercel(),
+  integrations: [react()]
 });
